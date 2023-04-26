2929f3c20e6df2abdac47164224da4c599d9307f
RajaRath55 <72096878+RajaRath55@users.noreply.github.com>
2023-04-26 11:33:56 +0530

    Update deploy-model-training.groovy

--- mlops/devops-pipelines/JenkinsPipelines/deploy-model-training.groovy
+++ mlops/devops-pipelines/JenkinsPipelines/deploy-model-training.groovy
@@ -1,67 +1,148 @@
-pipeline {
- withEnv(['AZURE_SUBSCRIPTION_ID=SubscriptionID',
-        'AZURE_TENANT_ID=TenantId']) {
-    stage('init') {
-      checkout scm
-    }
-
+node {
     stage('InstallAzureMLCLI'){
-        sh '''
+        powershell(
+        '''
         az version
         az extension add -n ml -y
         az extension update -n ml
         az extension list
         '''
+        )
     }
+    stage('init') {
+      checkout scm
+    }
+    withCredentials([azureServicePrincipal(credentialsId: 'MLOps-Azure-Serviceprinciple',
+    subscriptionIdVariable: 'SubscriptionID',
+    clientIdVariable: 'AZURE_CLIENT_ID',
+    clientSecretVariable: 'AZURE_CLIENT_SECRET',
+    tenantIdVariable: 'Azure_TENANT_ID')]){
+        stage('CreateResourceGroup'){
 
-    
-    stage('Configure Azure ML Workspace'){
-    	def aml_workspace_name = 'mlw-$namespace-$postfix-$environment'
-    	def resource_group_name = 'rg-$namespace-$postfix-$environment'
-        withCredentials([usernamePassword(credentialsId: 'MLOps-ServiceConnection', passwordVariable: 'AZURE_CLIENT_SECRET', usernameVariable: 'AZURE_CLIENT_ID')]) {
-        sh '''
-        az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET -t $TenantId
-        az account set -s $SubscriptionID
-	az configure --defaults group=$resource_group_name workspace=$aml_workspace_name
-        '''
+            powershell( '''
+            az login --service-principal -u %AZURE_CLIENT_ID% -p %AZURE_CLIENT_SECRET% -t %Azure_TENANT_ID%
+            az account set -s %SubscriptionID%
+            az group create --location 'westeurope' --name 'rg-mlops-rajat-01-dev'
+            '''
+            )
+
+        }
+
+        stage('CreateAzureMLWorkspace') {
+            // login Azure
+            powershell(
+            '''
+            az login --service-principal -u %AZURE_CLIENT_ID% -p %AZURE_CLIENT_SECRET% -t %Azure_TENANT_ID%
+            az account set -s %AZURE_SUBSCRIPTION_ID%
+            Write-Host "Checking workspace mlw-mlops-rajat-01-dev" 
+            $wkspcs=$(az ml workspace list -g  'rg-mlops-rajat-01-dev' --query [].display_name -o tsv )
+            $ws_exists="false"
+            echo "found workspaces" $wkspcs
+            Foreach ($ws in $wkspcs)
+            {
+                if($ws -eq "mlw-mlops-rajat-01-dev")
+                {
+                $ws_exists="true"
+                echo "Workspace mlw-mlops-rajat-01-dev already exists"
+                break
+                }
+                
+            }
+
+            if($ws_exists -eq "false")
+            {
+            Write-Host "Creating Workspace mlw-mlops-rajat-01-dev rg-mlops-rajat-01-dev westeurope now .."
+            az ml workspace create --name 'mlw-mlops-rajat-01-dev' -g 'rg-mlops-rajat-01-dev' -l 'westeurope'
+            break
+            }
+                '''
+            )
+        }
+
+        stage('Create custom environment'){
+            powershell(
+            '''
+                    az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET -t $TenantId
+                    az account set -s $AZURE_SUBSCRIPTION_ID
+                    az configure --defaults group=rg-mlops-rajat-01-dev workspace='mlw-mlops-rajat-01-dev'
+                    az ml environment create --name 'taxi-train-env' --file 'mlops/azureml/train/train-env.yml'
+                    '''
+            )
+        }
+
+        stage('CreateMLCompute'){
+            powershell(
+            '''
+                    az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET -t $TenantId
+                    az account set -s $AZURE_SUBSCRIPTION_ID
+                    az configure --defaults group=rg-mlops-rajat-01-dev workspace='mlw-mlops-rajat-01-dev'
+                    $compute_name=$(az ml compute show -n 'cpu-cluster' --query name -o tsv)
+                    if ($null -eq $compute_name)
+                    {
+                        Write-Host "Compute does not exists. Creating the cluster..."
+                        az ml compute create --name 'cpu-cluster' --type amlcompute --size 'STANDARD_D2_V2' --min-instances 0 --max-instances 1 --tier dedicated
+                    }
+
+                    else
+                    {
+                        Write-Host "Compute exists. Skipping cluster creation."
+                    }
+                    '''
+            )
+        }
+
+        stage('Register data'){
+            powershell(
+            '''
+                    az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET -t $TenantId
+                    az account set -s $AZURE_SUBSCRIPTION_ID
+                    az configure --defaults group=rg-mlops-rajat-01-dev workspace='mlw-mlops-rajat-01-dev'
+                    az ml data create --file 'mlops/azureml/train/data.yml' --name 'taxi-data' --type 'uri_file'
+            '''
+            )
+        }
+
+        stage('Run Pipeline'){
+            powershell(
+            '''
+                    az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET -t $TenantId
+                    az account set -s $AZURE_SUBSCRIPTION_ID
+                    az configure --defaults group=rg-mlops-rajat-01-dev workspace='mlw-mlops-rajat-01-dev'
+                    $run_id=$(az ml job create -f 'mlops/azureml/train/pipeline.yml' \
+                    --set experiment_name='dev_taxi_fare_train' \
+                    inputs.subscriptionId=%AZURE_SUBSCRIPTION_ID% \
+                    inputs.rg_group='rg-mlops-rajat-01-dev' \
+                    inputs.workspace='mlw-mlops-rajat-01-dev' \
+                    display_name=dev_taxi_fare_run_rajat --query name -o tsv)
+
+                    if($null -eq $run_id)
+                    {
+                        Write-Host "Job creation failed"
+                        exit 3
+                    }
+
+                    az ml job show -n $run_id --web
+                    $status=$(az ml job show -n $run_id --query status -o tsv)
+
+                    if($null -eq $status)
+                    {
+                        Write-Host "Status query failed"
+                        exit 4
+                    }
+
+                    while($status -ne "Completed")
+                    {
+                        sleep 15 
+                        $status=$(az ml job show -n $run_id --query status -o tsv)
+                        Write-Host $status
+                    }
+                    if($status -ne "Completed")
+                    {
+                        Write-Host "Training Job failed or canceled"
+                        exit 3
+                    }
+            '''
+            )
         }
     }
-  
-  
-    stage('Register Azure ML environment') {
-    def  aml_workspace_name = 'mlw-$namespace-$postfix-$environment'
-    def resource_group_name = 'rg-$namespace-$postfix-$environment'
-      // generate version, it's important to remove the trailing new line in git describe output
-      withCredentials([usernamePassword(credentialsId: 'MLOps-ServiceConnection', passwordVariable: 'AZURE_CLIENT_SECRET', usernameVariable: 'AZURE_CLIENT_ID')]) {
-        // login Azure
-        sh '''
-          az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET -t $AZURE_TENANT_ID
-          az account set -s $AZURE_SUBSCRIPTION_ID
-	  az ml environment create --file "mlops/azureml/train/train-env.yml"
-	  az ml environment create --name $environment_name --file "mlops/azureml/train/train-env.yml"
-        '''
-      }
-    }
-
-    stage('Create ML Compute'){
-    	def  aml_workspace_name = 'mlw-$namespace-$postfix-$environment'
-    	def resource_group_name = 'rg-$namespace-$postfix-$environment'
-        withCredentials([usernamePassword(credentialsId: 'MLOps-ServiceConnection', passwordVariable: 'AZURE_CLIENT_SECRET', usernameVariable: 'AZURE_CLIENT_ID')]) {
-        sh '''
-        az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET -t $TenantId
-        az account set -s $SubscriptionID
-	az configure --defaults group=$resource_group_name workspace=$aml_workspace_name
-        compute_name=$(az ml compute show -n $cluster_name --query name -o tsv)
-        if [[ -z "$compute_name" ]]
-        then
-          echo "Compute does not exists. Creating the cluster..."
-          az ml compute create --name $cluster_name --type amlcompute --size $size --min-instances $min_instances --max-instances $max_instances --tier $cluster_tier
-        else
-          echo "Compute exists. Skipping cluster creation."
-          exit 0
-        fi
-        '''
-        }
-    }
-  }
 }
